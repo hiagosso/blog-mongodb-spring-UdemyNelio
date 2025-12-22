bb04cab6ca8d0bbafd790d28ec9011ee7a0862f1
hiago <souza8hiago@gmail.com>
2025-12-22 18:41:26 -0300

    Atualização de usuário com PUT

--- src/main/java/com/hiagodev/blogMongo/resources/UserResource.java
+++ src/main/java/com/hiagodev/blogMongo/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,14 @@ public class UserResource {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable String id) {
         userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+     @PutMapping("/{id}")
+    public ResponseEntity<User> update(@RequestBody UserDTO userdto,@PathVariable String id) {
+        User obj = userService.fromDto(userdto);
+        obj.setId(id);
+        obj = userService.update(obj);
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/hiagodev/blogMongo/service/UserService.java
+++ src/main/java/com/hiagodev/blogMongo/service/UserService.java
@@ -17,25 +17,35 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return userRepository.findAll();
     }
 
-    public User findById(String id){
+    public User findById(String id) {
         Optional<User> user = userRepository.findById(id);
-       return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
-    public User insert(User obj){
+    public User insert(User obj) {
         return userRepository.insert(obj);
     }
 
-     public void delete(String id){
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return userRepository.save(newObj);
+    }
+    public void updateData(User newObj, User obj){
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    public void delete(String id) {
         findById(id);
         userRepository.deleteById(id);
     }
 
-    public User fromDto(UserDTO obj){
-        return new User(obj.getId(),obj.getName(),obj.getEmail());
+    public User fromDto(UserDTO obj) {
+        return new User(obj.getId(), obj.getName(), obj.getEmail());
     }
 }
