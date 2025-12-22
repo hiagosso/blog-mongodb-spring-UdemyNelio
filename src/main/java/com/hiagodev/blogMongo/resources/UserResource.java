5572b9974df6b2897bcdf0b6683540540b18113e
hiago <souza8hiago@gmail.com>
2025-12-22 18:19:05 -0300

    Inserção de usuário com POST

--- src/main/java/com/hiagodev/blogMongo/resources/UserResource.java
+++ src/main/java/com/hiagodev/blogMongo/resources/UserResource.java
@@ -7,12 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hiagodev.blogMongo.domain.User;
 import com.hiagodev.blogMongo.dto.UserDTO;
 import com.hiagodev.blogMongo.service.UserService;
+
 
 
 
@@ -36,5 +39,11 @@ public class UserResource {
         return ResponseEntity.ok(new UserDTO(obj));
     }
     
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody UserDTO userdto) {
+        User obj = userService.fromDto(userdto);
+        obj = userService.insert(obj);
+        return ResponseEntity.status(201).build();
+    }
     
 }
--- src/main/java/com/hiagodev/blogMongo/service/UserService.java
+++ src/main/java/com/hiagodev/blogMongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hiagodev.blogMongo.domain.User;
+import com.hiagodev.blogMongo.dto.UserDTO;
 import com.hiagodev.blogMongo.repository.UserRepository;
 import com.hiagodev.blogMongo.service.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = userRepository.findById(id);
        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return userRepository.insert(obj);
+    }
+
+    public User fromDto(UserDTO obj){
+        return new User(obj.getId(),obj.getName(),obj.getEmail());
     }
 }
