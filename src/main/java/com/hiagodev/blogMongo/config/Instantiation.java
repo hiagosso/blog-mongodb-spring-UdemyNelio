557a35f6b0d5b88b501d86b76171e2fed66deb33
hiago <souza8hiago@gmail.com>
2025-12-22 20:55:18 -0300

    Criando entity Post com User aninhado,Projeção dos dados do autor com DTO

--- src/main/java/com/hiagodev/blogMongo/config/Instantiation.java
+++ src/main/java/com/hiagodev/blogMongo/config/Instantiation.java
@@ -1,12 +1,17 @@
 package com.hiagodev.blogMongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.hiagodev.blogMongo.domain.Post;
 import com.hiagodev.blogMongo.domain.User;
+import com.hiagodev.blogMongo.dto.AuthorDTO;
+import com.hiagodev.blogMongo.repository.PostRepository;
 import com.hiagodev.blogMongo.repository.UserRepository;
 
 @Configuration
@@ -15,16 +20,28 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
         
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
-        
+        postRepository.deleteAll();
+
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-    
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
+        
+        Post p1 = new Post(null, sdf.parse("25/03/2018"), "partiu viagem!!", "vou viajar para SP.", new AuthorDTO(maria));
+        Post p2 = new Post(null, sdf.parse("24/06/2018"), "Estou de volta!!", "voltei da viagem de SP.", new AuthorDTO(maria));
+        Post p3 = new Post(null, sdf.parse("25/12/2018"), "feliz natal!", "bom natl para todos.",new AuthorDTO(bob));
+        postRepository.saveAll(Arrays.asList(p1,p2,p3));
+
     }
 
 }
