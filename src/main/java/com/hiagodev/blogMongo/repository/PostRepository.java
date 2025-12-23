f9af860f297e112e51f21b83321207d1ce9c4359
hiago <souza8hiago@gmail.com>
2025-12-22 23:44:10 -0300

    Consulta com vários critérios

--- src/main/java/com/hiagodev/blogMongo/repository/PostRepository.java
+++ src/main/java/com/hiagodev/blogMongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.hiagodev.blogMongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -9,12 +10,14 @@ import org.springframework.stereotype.Repository;
 import com.hiagodev.blogMongo.domain.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post,String> {
+public interface PostRepository extends MongoRepository<Post, String> {
 
     @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
-	List<Post> searchTitle(String text);
-    
+    List<Post> searchTitle(String text);
 
     // metodo com query method
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
