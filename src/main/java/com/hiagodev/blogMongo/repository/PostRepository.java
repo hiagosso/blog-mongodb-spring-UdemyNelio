b73eeabe3f08017acb425e44cf80b2c3db20c155
hiago <souza8hiago@gmail.com>
2025-12-22 23:13:10 -0300

    Consulta simples com @Query

--- src/main/java/com/hiagodev/blogMongo/repository/PostRepository.java
+++ src/main/java/com/hiagodev/blogMongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.hiagodev.blogMongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.hiagodev.blogMongo.domain.Post;
@@ -10,5 +11,10 @@ import com.hiagodev.blogMongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
 
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+    
+
+    // metodo com query method
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
