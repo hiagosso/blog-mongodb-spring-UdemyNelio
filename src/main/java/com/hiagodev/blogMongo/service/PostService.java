521f5dcdcd9bee7908a6becb5907401a59772f2c
hiago <souza8hiago@gmail.com>
2025-12-22 21:46:13 -0300

    Obtendo um post por id

---
+++ src/main/java/com/hiagodev/blogMongo/service/PostService.java
@@ -0,0 +1,22 @@
+package com.hiagodev.blogMongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hiagodev.blogMongo.domain.Post;
+import com.hiagodev.blogMongo.repository.PostRepository;
+import com.hiagodev.blogMongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+    
+    @Autowired
+    private PostRepository postRepository;
+    
+    public Post findById(String id) {
+        Optional<Post> post = postRepository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
