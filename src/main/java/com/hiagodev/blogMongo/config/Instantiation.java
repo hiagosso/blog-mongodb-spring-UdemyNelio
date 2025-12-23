be9eb68efb9130ab2f0abe2c1e6d015ffd7a4594
hiago <souza8hiago@gmail.com>
2025-12-22 22:03:42 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/hiagodev/blogMongo/config/Instantiation.java
+++ src/main/java/com/hiagodev/blogMongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.hiagodev.blogMongo.domain.Post;
 import com.hiagodev.blogMongo.domain.User;
 import com.hiagodev.blogMongo.dto.AuthorDTO;
+import com.hiagodev.blogMongo.dto.CommentDTO;
 import com.hiagodev.blogMongo.repository.PostRepository;
 import com.hiagodev.blogMongo.repository.UserRepository;
 
@@ -25,7 +26,7 @@ public class Instantiation implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        
+
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
@@ -35,16 +36,29 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-        userRepository.saveAll(Arrays.asList(maria,alex,bob));
-        
-        Post p1 = new Post(null, sdf.parse("25/03/2018"), "partiu viagem!!", "vou viajar para SP.", new AuthorDTO(maria));
-        Post p2 = new Post(null, sdf.parse("24/06/2018"), "Estou de volta!!", "voltei da viagem de SP.", new AuthorDTO(maria));
-        Post p3 = new Post(null, sdf.parse("25/12/2018"), "feliz natal!", "bom natl para todos.",new AuthorDTO(bob));
-        postRepository.saveAll(Arrays.asList(p1,p2,p3));
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        maria.getPosts().addAll(Arrays.asList(p1,p2));
+        Post p1 = new Post(null, sdf.parse("25/03/2018"), "partiu viagem!!", "vou viajar para SP.",
+                new AuthorDTO(maria));
+        Post p2 = new Post(null, sdf.parse("24/06/2018"), "Estou de volta!!", "voltei da viagem de SP.",
+                new AuthorDTO(maria));
+        Post p3 = new Post(null, sdf.parse("25/12/2018"), "feliz natal!", "bom natl para todos.", new AuthorDTO(bob));
+
+        CommentDTO c1 = new CommentDTO("boa viagem mano", sdf.parse("25/03/2018"), new AuthorDTO(bob));
+        CommentDTO c4 = new CommentDTO("boa viagem mari", sdf.parse("25/03/2018"), new AuthorDTO(alex));
+        
+        CommentDTO c2 = new CommentDTO("feliz natal tambem", sdf.parse("25/12/2018"), new AuthorDTO(maria));
+        CommentDTO c3 = new CommentDTO("teve uma boa viagem?", sdf.parse("24/6/2018"), new AuthorDTO(alex));
+
+        p1.getComments().addAll(Arrays.asList(c1,c4));
+        p2.getComments().add(c3);
+        p3.getComments().add(c2);
+
+        postRepository.saveAll(Arrays.asList(p1, p2, p3));
+        maria.getPosts().addAll(Arrays.asList(p1, p2));
         bob.getPosts().add(p3);
-        userRepository.saveAll(Arrays.asList(maria,bob));
+        userRepository.saveAll(Arrays.asList(maria, bob));
+
     }
 
 }
--- src/main/java/com/hiagodev/blogMongo/domain/Post.java
+++ src/main/java/com/hiagodev/blogMongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.hiagodev.blogMongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.hiagodev.blogMongo.dto.AuthorDTO;
+import com.hiagodev.blogMongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,9 +21,12 @@ public class Post implements Serializable {
     private String body;
     private AuthorDTO author;
 
+    private List<CommentDTO> comments = new ArrayList<>();
+
+    
     public Post(){
     }
-
+    
     public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
@@ -28,19 +34,19 @@ public class Post implements Serializable {
         this.body = body;
         this.author = author;
     }
-
+    
     public String getId() {
         return id;
     }
-
+    
     public void setId(String id) {
         this.id = id;
     }
-
+    
     public Date getDate() {
         return date;
     }
-
+    
     public void setDate(Date date) {
         this.date = date;
     }
@@ -48,26 +54,29 @@ public class Post implements Serializable {
     public String getTitle() {
         return title;
     }
-
+    
     public void setTitle(String title) {
         this.title = title;
     }
-
+    
     public String getBody() {
         return body;
     }
-
+    
     public void setBody(String body) {
         this.body = body;
     }
-     public AuthorDTO getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
-
+    
     public void setAuthor (AuthorDTO author) {
         this.author = author;
     }
-
+    
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
     
     @Override
     public int hashCode() {
@@ -76,7 +85,7 @@ public class Post implements Serializable {
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
-
+    
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
